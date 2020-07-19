6aaaccd36e0c378a85a94bd7f74274cf3ee498a3
Elina <elluksm@gmail.com>
2020-07-20 00:23:50 +0300

    add cookie banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CookieConsent from "react-cookie-consent";
 import { Home } from "./views/Home/Home";
 import { Chart } from "./views/Chart/Chart";
 import "./App.sass";
@@ -7,6 +8,13 @@ import "./App.sass";
 function App() {
   return (
     <div className="app-container">
+      <CookieConsent
+        location="top"
+        style={{ background: "#ffffff", color: "#5724d9" }}
+        buttonStyle={{ background: "#5724d9", color: "#ffffff" }}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
       <Router>
         <Switch>
           <Route path="/chart" component={Chart} />
